87c9d3df4f1141198615c443765f46f152fcd9f3
alessandromasone <masonealessandro04@gmail.com>
2023-07-07 09:46:26 +0200

    Miglioramento eliminazione elemento
    Implementazione migliorie lato server e lato client della fase di eliminazione di un elemento

--- js/delete.js
+++ js/delete.js
@@ -3,19 +3,20 @@ function clickdeletebutton(itemId) {
     $.ajax({
         url: 'php/delete.php',
         method: 'POST',
-        data: {
-            itemId: itemId
-        },
-        success: function(response) {
-            console.log(response); // Risposta dal server
-
-            // Rimuovi l'elemento dalla sidebar o esegui altre azioni necessarie
-            const item = document.querySelector(`[data-attrib-id="${itemId}"]`);
-            if (item) {
-                item.remove();
+        data: { itemId: itemId },
+        success: function (response) {
+            if (response.status == 'success') {
+                // Rimuovi l'elemento dalla sidebar o esegui altre azioni necessarie
+                const item = document.querySelector(`[data-attrib-id="${itemId}"]`);
+                if (item) {
+                    item.remove();
+                }
+            } else {
+                console.error(response.data);
             }
+
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error(error);
         }
     });
--- php/delete.php
+++ php/delete.php
@@ -4,20 +4,37 @@ require_once('connect.php');
 
 // Verifica se la richiesta è di tipo POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Verifica se il parametro 'id' è impostato
+    // Verifica se il parametro 'itemId' è impostato
     if (isset($_POST['itemId'])) {
         $itemId = $_POST['itemId'];
 
-        // Elimina l'elemento dal database
+        // Prepara la query di eliminazione
         $sql = "DELETE FROM Directory WHERE ID = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param('i', $itemId);
-        $success = $stmt->execute();
 
-        if ($success) {
-            echo "Elemento eliminato con successo.";
+        // Esegui la query
+        if ($stmt->execute()) {
+            // Verifica se sono state eliminate righe dal database
+            if ($stmt->affected_rows > 0) {
+                $data = "Elemento eliminato con successo.";
+                $status = "success";
+            } else {
+                $data = "Impossibile trovare l'elemento da eliminare.";
+                $status = "error";
+            }
         } else {
-            echo "Impossibile eliminare l'elemento.";
+            $data = "Errore durante l'eliminazione dell'elemento.";
+            $status = "error";
         }
+    } else {
+        $data = "Parametro 'itemId' mancante.";
+        $status = "error";
     }
+} else {
+    $data = "Richiesta non valida.";
+    $status = "error";
 }
+
+// Richiama la funzione sendJsonResponse
+sendJsonResponse($data, $status);
